8a684f1e38e21ab6d4d27f5fdf056c033aefcbbe
evarist <evaristjuamet@gmail.com>
2019-01-27 13:15:19 +0100

    login implement

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -7,8 +7,8 @@ import { HttpClient } from '@angular/common/http';
 export class ApiService {
 
   // urlRegister = 'https://localhost:5001/api/users';
-  urlRegister = '/users';
-
+  urlRegister = '/api/users';
+  urlLogin = '/api/auth'
   constructor(private http: HttpClient) { }
 
   register(username, password) {
@@ -16,4 +16,27 @@ export class ApiService {
     return this.http.post(this.urlRegister, { username, password }).toPromise();
   }
 
+  login(username, password) {
+
+    const body = { username, password };
+    return new Promise((resolve, reject) => {
+      this.http.post(this.urlLogin, body)
+      .toPromise().then(() => {
+        reject('User o password not found');
+      }).catch(maybeNotAndError => {
+        if (maybeNotAndError.status === 200) {
+          console.log('status.error.200');
+         /*  const jwt = maybeNotAndError.error.text;
+          this.jwt = jwt;
+          localStorage.setItem('jwt', jwt); */
+          resolve(200);
+        } else if (maybeNotAndError.status === 401) {
+          reject('Wrong password');
+        } else {
+          reject('Try again');
+        }
+        console.log('fin login api');
+      });
+    });
+  }
 }
--- src/app/login-main/login-main.component.ts
+++ src/app/login-main/login-main.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-main',
   templateUrl: './login-main.component.html',
   styleUrls: ['./login-main.component.css']
 })
-export class LoginMainComponent implements OnInit {
+export class LoginMainComponent {
+  urlLogin: '/api/auth';
+  username: string;
+  password: string;
+  error: any;
 
-  constructor() { }
+  constructor(private api: ApiService, private router: Router) { }
 
-  ngOnInit() {
+  loginUser(){
+    const { username, password } = this;
+
+    this.api.login(username.trim(), password.trim())
+    .then((response) => {
+      console.log(response, 'entra en then de login');
+      this.error = undefined;
+      this.router.navigate(['/main']);
+    }).catch(error => {
+      this.error = error;
+    })
   }
 
 }
