df0c62faa74a27746b8591274a577e21be4d8347
evarist <evaristjuamet@gmail.com>
2019-01-27 12:52:41 +0100

    proxi cors solved

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  // urlRegister = 'https://localhost:5001/api/users';
+  urlRegister = '/users';
+
+  constructor(private http: HttpClient) { }
+
+  register(username, password) {
+    console.log(username, password, ' - registerFunctionApi.');
+    return this.http.post(this.urlRegister, { username, password }).toPromise();
+  }
+
 }
--- src/app/register-main/register-main.component.ts
+++ src/app/register-main/register-main.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-register-main',
   templateUrl: './register-main.component.html',
   styleUrls: ['./register-main.component.css']
 })
-export class RegisterMainComponent implements OnInit {
+export class RegisterMainComponent {
+  username: string;
+  password: string;
+  error: any;
+  valid: any;
 
-  constructor() { }
+  constructor(private api: ApiService) { }
 
-  ngOnInit() {
+  registerUser(){
+    const { username, password } = this;
+    if (username.trim() !== '' && password.trim() !== '') {
+      if (username.trim().length >= 4) {
+         this.api.register(username.trim(), password.trim()).then( result => {
+        console.log(result);
+        this.valid = result;
+      })
+      .catch(error => {
+        console.log(error);
+        this.error = error;        
+      });
+      }
+    }
   }
 
 }
